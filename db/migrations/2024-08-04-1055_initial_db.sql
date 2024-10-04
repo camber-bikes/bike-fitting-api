d6bf7709becb76b784d3d8ac928a1400bde2b93d
MrSpoony <kimi.loeffel@hispeed.ch>
2024-10-04 20:49:32 +0200

    feat(db): add inital schema

---
+++ db/migrations/2024-08-04-1055_initial_db.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS account(
+    id BIGSERIAL PRIMARY KEY,
+    uuid UUID UNIQUE NOT NULL,
+    height_cm smallint NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS scan(
+    id BIGSERIAL PRIMARY KEY,
+    uuid UUID UNIQUE NOT NULL,
+    account_id BIGINT NOT NULL,
+    result JSONB,
+    CONSTRAINT fk_account_id
+        FOREIGN KEY(account_id)
+        REFERENCES account(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TYPE STATUS AS ENUM ('new', 'done');
+
+CREATE TABLE IF NOT EXISTS image(
+    id BIGSERIAL PRIMARY KEY,
+    uuid UUID NOT NULL,
+    scan_id BIGINT NOT NULL,
+    status STATUS NOT NULL,
+    joints JSONB,
+    CONSTRAINT fk_scan_id
+        FOREIGN KEY(scan_id)
+        REFERENCES scan(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS video(
+    id BIGSERIAL PRIMARY KEY,
+    uuid UUID NOT NULL,
+    scan_id BIGINT NOT NULL,
+    status STATUS NOT NULL,
+    joints JSONB[],
+    CONSTRAINT fk_scan_id
+        FOREIGN KEY(scan_id)
+        REFERENCES scan(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
