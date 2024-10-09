b83f5e8f6a892782c0814c8317b5f4319049729d
Sven <me@thesven.dev>
2024-10-09 11:55:29 -0700

    feat(db): change db connection on prod env to unix socket

--- app/core/db.py
+++ app/core/db.py
@@ -1,6 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 import os
 
-engine = create_async_engine(
-    f"postgresql+asyncpg://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("POSTGRES_ENDPOINT")}/{os.getenv("POSTGRES_DB")}"
-)
+if os.getenv("ENV") == "PROD":
+    engine = create_async_engine(
+        f"postgresql+asyncpg://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@/{os.getenv("POSTGRES_DB")}"
+        f"?host=/cloudsql/{os.getenv("INSTANCE_CONNECTION_NAME")}"
+    )
+else:
+    engine = create_async_engine(
+        f"postgresql+asyncpg://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("POSTGRES_ENDPOINT")}/{os.getenv("POSTGRES_DB")}"
+    )
