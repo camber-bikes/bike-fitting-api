8a3827d205b24f5e224cdd2db1fd532608c5e667
MrSpoony <kimi.loeffel@hispeed.ch>
2024-10-09 20:41:02 +0200

    feat(api): add get requests for s3 and information

--- app/api/routes/persons.py
+++ app/api/routes/persons.py
@@ -1,22 +1,24 @@
 import uuid
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from sqlmodel import select
 from app.api.deps import SessionDep
 from app.apimodels import (
     CreatePersonInformation,
-    CreatePersonInformationResponse,
+    PersonInformationResponse,
+    ScanResponse,
 )
-from app.dbmodels import Person
+from app.dbmodels import Person, Scan
 
 
 router = APIRouter()
 
 
 @router.put("/information")
-async def create_person(
+async def create_person_information(
     session: SessionDep, body: CreatePersonInformation
-) -> CreatePersonInformationResponse:
+) -> PersonInformationResponse:
     """
-    Create person with given information
+    Create person with given information.
     """
 
     person = Person(uuid=uuid.uuid4(), name=body.name, height_cm=body.height_cm)
@@ -24,8 +26,47 @@ async def create_person(
     await session.commit()
     await session.refresh(person)
 
-    return CreatePersonInformationResponse(
+    return PersonInformationResponse(
         uuid=person.uuid,
         name=person.name,
         height_cm=person.height_cm,
     )
+
+
+@router.get("/{person_uuid}/information")
+async def get_person_information(
+    session: SessionDep, person_uuid: uuid.UUID
+) -> PersonInformationResponse:
+    """
+    Get person information.
+    """
+
+    statement = select(Person).where(Person.uuid == person_uuid)
+    person = await session.exec(statement)
+    person = person.first()
+    if person == None:
+        raise HTTPException(400, "person not found")
+
+    return PersonInformationResponse(
+        uuid=person.uuid,
+        name=person.name,
+        height_cm=person.height_cm,
+    )
+
+
+@router.get("/{person_uuid}/scans")
+async def get_scans(session: SessionDep, person_uuid: uuid.UUID) -> list[ScanResponse]:
+    """
+    Get all scans
+    """
+
+    statement = select(Person).where(Person.uuid == person_uuid)
+    person = await session.exec(statement)
+    person = person.first()
+    if person == None:
+        raise HTTPException(400, "person not found")
+
+    statement = select(Scan).where(Scan.person_id == person.id)
+    scans = await session.exec(statement)
+
+    return [ScanResponse(scan_uuid=scan.uuid) for scan in scans]
