4fb3eb55d6a6f8e6c6d61ce33cddac6dc45df66a
MarcEgliP <egli@puzzle.ch>
2024-10-10 01:22:19 +0200

    Whitelist all hosts

--- app/main.py
+++ app/main.py
@@ -5,6 +5,11 @@ load_dotenv()
 from fastapi import FastAPI
 from app.api.main import api_router
 
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+
+app.add_middleware(
+    TrustedHostMiddleware, allowed_hosts=["*"]
+)
 
 app = FastAPI(title="Bike Fitting API", openapi_url="/api/openapi.json")
 
